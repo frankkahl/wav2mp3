fd520615d25909893e15ea568af64d4112336fdd
Frank Kahl <frank.kahl@gmx.net>
2019-07-14 20:39:59 +0200

    - Bug fixed: check_pthread_error.cpp: did not compile under Windows anymore - superfluous includes removed

--- src/check_pthread_error.cpp
+++ src/check_pthread_error.cpp
@@ -6,21 +6,30 @@
 
 using namespace std;
 
-// it is super annoying that different functions are used for a thread save strerror version
-// under Windows and Linux
+// under Linux the dreaded _GNU_SOURCE is defined by default
+// and insanely enough also required by the libstd++ header to work properly
+// This means that according to the documentation I cannot use the POSIX compliant version of strerror_r
+
 #if defined(_WIN32)
 #  define strerror_r(errno, buf, len) strerror_s(buf, len, errno) 
 #endif
 
 #define MAX_ERROR_STRING_LENGTH 256
-void check_pthread_error(int errnum, const string pthread_function_name) {
+void check_pthread_error(int errnum, const string &pthread_function_name) {
     if (errnum) {
         ostringstream ss;
-        char errmsg[MAX_ERROR_STRING_LENGTH + 1];
+        char errmsg[MAX_ERROR_STRING_LENGTH];
 	// it turns out that under Ubuntu Linux using g++ 9.1
 	// the GNU version of strerror_r is used by default
 	// which returns a pointer to the error string
-        char *msg = strerror_r(errnum, errmsg, MAX_ERROR_STRING_LENGTH);
+#if defined(_WIN32)
+	strerror_s(errmsg, MAX_ERROR_STRING_LENGTH, errnum);
+	char *msg = errmsg;
+#elif defined(_GNU_SOURCE)
+        char * msg = strerror_r(errnum, errmsg, MAX_ERROR_STRING_LENGTH);
+#else
+	char *msg = "<unsupported platform: cannot retrieve error string>";
+#endif
         ss << "Pthread function \"" << pthread_function_name;
         ss << "\" failed with error: " << string(msg) << " (" << errnum << ")" << endl;
         cout << ss.str();
--- src/check_pthread_error.h
+++ src/check_pthread_error.h
@@ -2,6 +2,7 @@
 #define CHECK_PTHREAD_ERROR_H
 
 #include <string>
-extern void check_pthread_error(int errnum, const std::string pthread_function_name);
+
+extern void check_pthread_error(int errnum, const std::string &pthread_function_name);
 
 #endif // CHECK_PTHREAD_ERROR_H
